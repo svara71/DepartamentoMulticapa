35fff0b6b3618ffab203d945f439ad6c36c406b3
svara71 <svara71@gmail.com>
2017-03-07 12:48:33 +0100

    Add files via upload

---
+++ Documentacion/phpDoc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","CalculaEdad()"],["c","DBPDO"],["c","Departamento"],["c","DepartamentoPDO"],["c","PDOStatement"],["c","Traversable"],["c","Usuario"],["c","UsuarioPDO"],["f","validarCampoAlfanumerico()"],["f","validarCampoNumero()"],["f","validarCampoTexto()"],["f","validarFechaAnt()"]];
